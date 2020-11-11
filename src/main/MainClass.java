af4b7f42e67fe0bf25281c3f3f2fafb85844cc0e
Julius Schinschke <mail@julius-schinschke.de>
2020-11-11 10:23:43 +0100

    add Gregorian Calender

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,8 +31,9 @@ public class MainClass {
     }
 
     private boolean isLeap(int year) {
-        return ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0));
+        return ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)) && new GregorianCalendar().isLeapYear(year);
     }
+
 }
 
 
