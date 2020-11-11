43cf32846c171dcbe9cfe47d8191438f4a325b7e
Julius Schinschke <mail@julius-schinschke.de>
2020-11-11 10:52:33 +0100

    Refactor Methods

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,34 +5,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.GregorianCalendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MainClass {
 
-    private static final Logger logger = Logger.getLogger(MainClass.class.getName());
 
     public static void main(String[] args) throws Exception {
-        MainClass newClass = new MainClass();
-        //logger.log(Level.INFO, "{0}", newClass.getValueSwing());
-        //logger.log(Level.INFO, "{0}", newClass.getValueStream());
-        int year = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        boolean isLeap = newClass.isLeap(year);
-        logger.log(Level.INFO, "is leap? Answer: {0}", isLeap);
+        //System.out.println(getValueSwing());
+        //System.out.println(getValueStream());
+        leapYearTest();
     }
 
-    public int getValueSwing() {
+    private static void leapYearTest() throws IOException {
+        int year = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        System.out.println("is leap with Algebra and Calender test? Answer: " + isLeapWithAlgebra(year));
+        System.out.println("is leap with if-Statement? Answer: " + isLeapWithIfStatement(year));
+    }
+
+    public static int getValueSwing() {
         return Integer.parseInt(JOptionPane.showInputDialog("Please enter a number: "));
     }
 
-    public int  getValueStream() throws NumberFormatException, IOException {
-        logger.log(Level.INFO, "Please enter a number: ");
+    public static int  getValueStream() throws NumberFormatException, IOException {
+        System.out.println("Please enter a number: ");
         return Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
     }
 
-    private boolean isLeap(int year) {
+    private static boolean isLeapWithIfStatement(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0)
+                return true;
+            if (year % 400 == 0)
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean isLeapWithAlgebra(int year) {
         return ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)) && new GregorianCalendar().isLeapYear(year);
     }
+
+
 
 }
 
